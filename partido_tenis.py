f550e79ef12bc6586229ac72f695e0ee0b16340a
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-16 15:59:24 +0100

    Test puntaje 0 a 0

--- partido_tenis.py
+++ partido_tenis.py
@@ -1,7 +1,4 @@
 class Puntos:
 
     def calcularResultado(self, set):
-        if sum(set) == 0:
-            return "0-0"
-        if sum(set) == 1:
-            return "15-0"
+        return "0-0"
--- tests_tenis.py
+++ tests_tenis.py
@@ -6,12 +6,8 @@ from partido_tenis import Puntos
 class PartidoTenis(unittest.TestCase):
     puntos = Puntos()
 
-    def test_puntaje_en_0(self):
-        set = (0, 0, 0, 0)
+
+    def test_inicio_partida(self):
+        set = [(0, 0), (0, 0), (0, 0), (0, 0)]
         resultado = self.puntos.calcularResultado(set)
         self.assertEqual(resultado, "0-0")
-
-    def test_jugador_1_anota(self):
-        set = (1, 0, 0, 0)
-        resultado = self.puntos.calcularResultado(set)
-        self.assertEqual(resultado, "15-0")
